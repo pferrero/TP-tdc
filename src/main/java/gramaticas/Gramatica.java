a2eb325e16a128f0ae13f22f4287e1ce9cfc820f
Pablo Ferrero <ferreronicolas58@gmail.com>
2020-10-30 15:24:48 -0300

    Variables Nulleables Empiezo el método para buscar las variables nulleables.

--- src/main/java/gramaticas/Gramatica.java
+++ src/main/java/gramaticas/Gramatica.java
@@ -27,6 +27,9 @@ public class Gramatica {
         this.variables.add(prod.getLadoIzquierdo().charAt(0));
         for (int i = 0; i < prod.getLadoDerecho().length(); i++) {
             char caracter = prod.getLadoDerecho().charAt(i);
+            // Agregar método Produccion.esTerminal()
+            // Character.isLowerCase(char) va a dar false con algunos terminales
+            // como { o ()
             if (Character.isLowerCase(caracter)) {
                 terminales.add(caracter);
             } else {
@@ -34,6 +37,18 @@ public class Gramatica {
             }
         }
         producciones.add(prod);
+    }
+    
+    public Set<String> variablesNulleables() {
+        Set<String> nulleables = new HashSet<>();
+        // Caso base: Son nulleables las variables que tienen una producción
+        // A -> \epsilon
+        nulleables.addAll(producciones
+                .stream()
+                .filter(p -> p.getLadoDerecho().equals(Produccion.EPSILON))
+                .map(Produccion::getLadoIzquierdo)
+                .collect(Collectors.toSet()) );
+        return nulleables;
     }
     /*
     public boolean esNulleable(char varA) {
