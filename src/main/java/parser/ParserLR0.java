b33a7544dcc910a4e32e2e26124cc993408713dc
Administrator <Administrator@MININT-I38HK4F>
2020-12-02 20:43:30 -0300

    Creacion de Clase ParserLR0

---
+++ src/main/java/parser/ParserLR0.java
@@ -0,0 +1,89 @@
+package parser;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.InputMismatchException;
+import java.util.Iterator;
+import java.util.Set;
+
+import Utilities.ReadFile;
+import Utilities.Validator;
+import gramaticas.Gramatica;
+
+public class ParserLR0 {
+	
+	private Gramatica gramatica;
+	private HashSet<Item> items;
+	
+	public ParserLR0(Gramatica gramatica)
+	{
+		this.gramatica = gramatica;
+	}
+	
+	public HashSet<Item> getItems() 
+	{   if (this.items == null)
+    {
+        this.items = new HashSet<Item>();
+
+        this.items.add(new Item(this.gramatica.getProducciones().stream().skip(0).findFirst().get(), 0));
+        this.items.add(new Item(this.gramatica.getProducciones().stream().skip(0).findFirst().get(), 1));
+
+        Iterator<gramaticas.Produccion> producciones = this.gramatica.getProducciones().iterator();
+        while(producciones.hasNext())
+        {
+        	gramaticas.Produccion prod = producciones.next();
+            for (int i = 0; i < prod.getLadoDerecho().length(); i++)
+            {
+                this.items.add(new Item(prod, i));
+            }
+        }
+    }
+    return this.items;
+	}
+	
+	//clausuraItem(Item)
+	//IrA(HashSet<Item>, Caracter)
+	//accion()
+	//reduce()
+	//shift()
+	
+	public static void main(String[] args) {
+		String test = "abc";
+		System.out.println(test.charAt(0));
+		
+		ArrayList<String> lista = new ArrayList<String>();
+		
+		lista.add("X{23");
+		lista.add("X02");
+		lista.add("X03");
+		lista.add("X05");
+		lista.add("X13");
+		lista.add("X01");
+		lista.add("X10");
+		lista.add("X04");
+		lista.add("X0");
+		lista.add("X12");
+		lista.add("X6");
+		lista.add("X8");
+		lista.add("X7");
+		
+	System.out.println(lista);
+		
+		
+	}
+	
+	public void generarGramatica(String filePath) 
+	{
+		ReadFile reader = new ReadFile();
+		Validator validator = new Validator();
+		reader.assingFile(filePath);
+		for(int i = 0; i < reader.linesNumber(); i++) 
+		{
+			String linea = reader.getLine(i);
+			if(!validator.isValid(linea, Produccion.EXP_PRODUCCION))
+				throw new InputMismatchException("Error al leer el archivo. Produccion invalida");
+			this.gramatica.agregarProduccion(new gramaticas.Produccion(linea));
+		}
+	}
+	
+}
